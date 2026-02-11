408f6fe9b966c2c83b86fa34eb143b2980e660f1
Avi <avraham2@gmail.com>
2026-02-11 13:24:47 +0200

    Upgraded the batch file to run over any version of dist SitemapParser Fixes #13

--- zip-resources/ParseSitemap.bat
+++ zip-resources/ParseSitemap.bat
@@ -1,3 +1,14 @@
 @echo off
 cls
-java -jar -Dlogback.configurationFile=logback.xml SitemapParser-v0.6.jar
+
+setlocal
+for /f "delims=" %%f in ('dir /b /a:-d /o-d "SitemapParser-v*.jar" 2^>nul') do (
+    set "JAR=%%f"
+    goto :run
+)
+
+echo No SitemapParser jar found in "%cd%".
+exit /b 1
+
+:run
+java -Dlogback.configurationFile=logback.xml -jar "%JAR%" %1
